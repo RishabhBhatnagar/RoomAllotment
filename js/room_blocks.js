d607f29079a1e176ac72d74cc257853a746658fa
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-09-16 18:24:36 +0530

    added room numbers dynamically

--- js/room_blocks.js
+++ js/room_blocks.js
@@ -18,14 +18,9 @@ function create_table(nod, container){
     for(i = 0; i<nod; i++){
         temp_row_data = wrap("td", ".");
         temp_row = wrap("tr", temp_row_data);
-        //window.alert(temp_row);
         code += temp_row;
     }
     code = wrap("table", code);
-    window.alert(code);
-    
-    document.getElementById(container).innerHTML = code;
-    document.write(code);
 }
 
 function bind_radio_listener(name){
@@ -37,5 +32,31 @@ function bind_radio_listener(name){
     }
 }
 
+function inflate_blocks(name){
+    all_room_nos = {
+        "classroom" : ["23","32"], 
+        "lab"       : ["100", "101"], 
+        "others"    : ["41", "14"]
+    };
+    div_ele = document.getElementById("list_blocks");   
+    
+    room_nos = all_room_nos[name];
+    for(index in room_nos){
+        //Create an button dynamically.   
+        var element = document.createElement("input");
+        //Assign different attributes to the element. 
+        element.type = "button";
+        element.value = room_nos[index];
+        element.name = room_nos[index];
+        element.onclick = function() {
+            alert(element.value);
+        };
+
+        //Append the element in page (in span).  
+        div_ele.appendChild(element);
+    }
+}
 bind_radio_listener("block");
+
+inflate_blocks("classroom");
 create_table(12);
