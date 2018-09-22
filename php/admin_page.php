1350b925908ff23f04bb1a805f4a870a1087596a
Mandar Acharekar <mandarmva8055@gmail.com>
2018-09-22 14:12:23 +0530

    dynamically added card view

--- php/admin_page.php
+++ php/admin_page.php
@@ -6,19 +6,53 @@
 <body>
 	<center>
 		<?php 
-			$name="jhvsdfjhvd";
 
+			function getCards( $commName,$commHead)
+			{
+				return 
+				sprintf(
+					"<td>
+						<div class=indvCards>
+							Name Of Commitee
+							<h2> %s </h2>
+							Head Of Commitee
+						    <h2> %s </h2>
+					    </div>
+					</td>", 
+					$commName, $commHead
+			    );
+			}
+
+			$noOfCards_perLine=3;
+			$name="CODE-X";
+			$cards=array(getCards("CODE_X","VERRUS"),getCards("CSIX","VERxvxvc"),getCards("IEEE","VEcsdcsdcUS"),getCards("ECELL","VEdS"),
+			getCards("CODE_X","VERRUS"),getCards("CSIX","VERxvxvc"),getCards("IEEE","VEcsdcsdcUS"),getCards("ECELL","VEdcsdcRRUS"),
+			getCards("CODE_X","VERRUS"),getCards("CSIX","VERxvxvc"),getCards("IEEE","VEcsdcsdcUS"),getCards("ECELL","VEdcsdcRRUS"),
+			getCards("CODE_X","VERRUS"),getCards("CSIX","VERxvxvc"),getCards("IEEE","VEcsdcsdcUS"),getCards("ECELL","VEdcsdcRRUS"));
+
+			$noOfCards=sizeof($cards);
+			echo '<script>console.log("'.$noOfCards.'")</script>';
 			print("<table>");
-			$i = 0;
-			while ($i<5) {
-
-					print("<tr>");
-					print("<td><div class=\"indvCards\"> Name Of Commitee<h2>".$name."</h2></div></td>");
-					print("<td><div class=\"indvCards\"> Date of booking<h2>".$name."</h2></div></td>");
-					print("<td><div class=\"indvCards\"> Duration<h2>".$name."</h2></div></td>");
-					print("<td><div class=\"indvCards\"> Event Description<h2>".$name."</h2></div></td>");
-					print("</tr><br>");
-					$i++;
+			
+			$cardsleft = $noOfCards;
+			while ($cardsleft > 0) {
+				print("<tr>");
+				if($cardsleft > $noOfCards_perLine){
+					# SHOW ALL NUMBER OF CARDS.
+					for($i = 0; $i<$noOfCards_perLine; $i++){
+						print($cards[$i + $noOfCards - $cardsleft]);
+						echo '<script>console.log("'.($i + $noOfCards - $cardsleft).'")</script>';
+					}
+					$cardsleft -= $noOfCards_perLine;
+					echo '<script>console.log("next")</script>';
+				} else{
+					while($cardsleft != 0){
+						print($cards[$noOfCards - $cardsleft]);
+						echo '<script>console.log("'.($noOfCards - $cardsleft).'")</script>';
+						$cardsleft -= 1;
+					}
+				}
+				print("</tr>");
 				}
 			print("</table>");
 
