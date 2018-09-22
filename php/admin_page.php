bda797c2174547bb6353823a510a2a9aade424d0
Mandar Acharekar <mandarmva8055@gmail.com>
2018-09-22 12:12:57 +0530

    admin page --mandar

---
+++ php/admin_page.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/admin_page.css">
+</head>
+<body>
+	<center>
+		<?php 
+			$name="jhvsdfjhvd";
+
+			print("<table>");
+			$i = 0;
+			while ($i<5) {
+
+					print("<tr>");
+					print("<td><div class=\"indvCards\"> Name Of Commitee<h2>".$name."</h2></div></td>");
+					print("<td><div class=\"indvCards\"> Date of booking<h2>".$name."</h2></div></td>");
+					print("<td><div class=\"indvCards\"> Duration<h2>".$name."</h2></div></td>");
+					print("<td><div class=\"indvCards\"> Event Description<h2>".$name."</h2></div></td>");
+					print("</tr><br>");
+					$i++;
+				}
+			print("</table>");
+
+		?>
+		
+
+		
+
+	
+
+
+		
+	</center>
+	
+</body>
+
+</html>
