f1f71195405533fb3721c3874e778586de62d948
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-10-05 22:14:03 +0530

    added side nav and fragments.

---
+++ src/php/side_navigation.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <style>
+            body{
+                width:90%;
+                height:95%;
+                border:6px outset orange;
+                background:#000000
+            }
+        </style>
+    </head>
+    <body>
+        <div style="">
+            <a method="get" href="previous_events.php?id=12" target="main_frame" >Previous Events</a><br>
+            <a href = "pending_events.php" target="main_frame">Pending Events</a><br>
+            <a href = "new_events.php" target="main_frame">New Events</a>
+        </div>
+    </body>
+<html>
