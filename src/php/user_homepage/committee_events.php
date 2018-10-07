581ec72f5ac986d32960af412ea3774321d6b6a9
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-10-07 13:09:21 +0530

    added particle login

--- src/php/user_homepage/committee_events.php
+++ src/php/user_homepage/committee_events.php
@@ -1,11 +1,13 @@
 <?php
     include "../../data/get_data.php";
+    
+    //getting all committees' names in array
     $committees = get_table_data_query("select comm_name from user where comm_name != 'admin';");
+    
     for($i = 0; $i < count($committees); $i++){
         
-        if(isset($_GET["c_name"]) and $_GET["c_name"] = $committees[$i]["comm_name"]."true"){
+        if(isset($_GET["c_name"]) and $_GET["c_name"] == $committees[$i]["comm_name"]."true"){
             echo "*".$_GET["c_name"]."*";
         }
     }
-    print($_GET["c_name"]);
 ?>
