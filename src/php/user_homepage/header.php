ae9015afcc39b015b0074b4f28c77a9703bdf2cc
Mandar Acharekar <mandarmva8055@gmail.com>
2018-10-12 10:26:01 +0530

    header sorted.

--- src/php/user_homepage/header.php
+++ src/php/user_homepage/header.php
@@ -1,62 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="../../css/assets/demo.css">
-	<link rel="stylesheet" href="../../css/assets/header-login-signup.css">
-	<link rel="stylesheet" href="../../css/assets/header-user-dropdown.css">
-	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="../../css/header.css">
 
 </head>
 <body>
-	<header class="header-login-signup">
-
-	<div class="header-limiter">
-
-		<h1>Room <span> Allotment</span></h1>
-
-		<ul>
-			<li><a href="#" class="selected">LogOut</a></li> 	
-		</ul>
-
+	<div class="header">
+	  <a class="logo"><h2>Room <span class="span-text"> Allotment</span></h2> </a>
+	  <div class="header-right">
+	    <a class="active" href="#home">USER</a>
+	    <a href="#contact">LOGOUT</a>
+	    
+	  </div>
 	</div>
 
-</header>
-
-	<div class="header-fixed-placeholder"></div>
-
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>
-
-	$(document).ready(function(){
-
-		var showHeaderAt = 150;
-
-		var win = $(window),
-				body = $('body');
-
-		// Show the fixed header only on larger screen devices
-
-		if(win.width() > 600){
-
-			// When we scroll more than 150px down, we set the
-			// "fixed" class on the body element.
-
-			win.on('scroll', function(e){
-
-				if(win.scrollTop() > showHeaderAt) {
-					body.addClass('fixed');
-				}
-				else {
-					body.removeClass('fixed');
-				}
-			});
-
-		}
-
-	});
-
-</script>
 
 
 </body>
--- src/php/user_homepage/side_navigation.php
+++ src/php/user_homepage/side_navigation.php
@@ -4,29 +4,23 @@
             body{
                 width:90%;
                 height:95%;
-                border:6px outset orange;
-                background:#ffffff
-            }
-        </style>
-    </head>
-    <body>
-        <div style="">
-            <a method="get" href="previous_events.php?id=12" target="main_frame" id="previous_events" name="previous_events" style="display:none">Previous Events</a><br>
-            <a href = "pending_events.php" target="main_frame" id = pending_events name = "pending_events" style="display:none">Pending Events</a><br>
-            <a href = "new_events.php" target="main_frame" id ="new_events" name = "new_events" style="display:none">New Events</a>
-            <?php
-                $_POST["who"] = "g";
-                include "../../data/get_data.php";
-                $committees = get_table_data_query("select distinct comm_name from user where comm_name != 'admin'");
-                for($i = 0; $i < count($committees); $i++){
-                    echo "
-                    <div id=\"div_".$committees[$i]["comm_name"]."\" style=\"display:none\">
-                        <input type=checkbox  value=\"".$committees[$i]["comm_name"]."\">".$committees[$i]["comm_name"]."<br>
-                    </div>
-                    ";
-                }
-            ?>
-        </div>
+                background:#292c2f;
+                
+            }         </style>     </head>     <body>         <div style="">
+<a method="get" href="previous_events.php?id=12" target="main_frame"
+id="previous_events" name="previous_events" style="display:none">Previous
+Events</a><br>             <a href = "pending_events.php" target="main_frame"
+id = pending_events name = "pending_events" style="display:none">Pending
+Events</a><br>             <a href = "new_events.php" target="main_frame" id
+="new_events" name = "new_events" style="display:none">New Events</a>
+<?php                 $_POST["who"] = "g";                 include
+"../../data/get_data.php";                 $committees =
+get_table_data_query("select distinct comm_name from user where comm_name !=
+'admin'");                 for($i = 0; $i < count($committees); $i++){
+echo "                     <div id=\"div_".$committees[$i]["comm_name"]."\"
+style=\"display:none\">                         <input type=checkbox  value=\"
+".$committees[$i]["comm_name"]."\">".$committees[$i]["comm_name"]."<br>
+</div>                     ";                 }             ?>         </div>
         
         
         
