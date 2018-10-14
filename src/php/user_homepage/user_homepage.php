284a655ad22c726a0c1ba91102ecde9a06c86b37
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-10-14 14:41:17 +0530

    blocked unauthorised access, added gitignore, updated readme

--- src/php/user_homepage/user_homepage.php
+++ src/php/user_homepage/user_homepage.php
@@ -4,6 +4,18 @@
             1) Directly from login page when user is a guest.
             2) Validated person when user is either an admin or a committee head.
     -->
+    <?php
+        include "../../data/get_data.php";
+        session_start();
+        if(!isset($_SESSION["uid"])){
+            toast("Sorry, you are not authenticated, Login first");
+            echo "
+                <script>
+                    window.location.href = '../login.php';
+                </script>
+            ";
+        }
+    ?>
     <frameset rows="15%, *" border=0>
         <frame src="header.php" style="background-color: #292c2f; opacity: 0.9">
         <frameset cols="20%, *" border="0">
