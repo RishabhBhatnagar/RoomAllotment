6418679872ab0273b154d7dd3284e14336e199b7
Mandar Acharekar <mandarmva8055@gmail.com>
2018-10-20 12:26:03 +0530

    Side nav styling added

--- src/php/user_homepage/header.php
+++ src/php/user_homepage/header.php
@@ -1,27 +1,7 @@
-<!--
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="../../css/header.css">
-    </head>
-    <body>
-        <div class="header">
-          <a class="logo"><h2>Room <span class="span-text"> Allotment</span></h2> </a>
-          <div class="header-right">
-              <a class="active" href="#home">USER</a>
-              <a href="logout.php" id="logout">LOGOUT</a>
-          </div>
-        </div>
-    </body>
-
-</html>
--->
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="../../css/header.css">
+        <link rel="stylesheet" type="text/css" href="../../css/header.css">
     </head>
     <body>
         <div class="header">
